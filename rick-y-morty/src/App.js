5671e7a79bd714c2357482a8d10108391413cd7f
cdmejiam94 <cdmejiam94@gmail.com>
2022-03-08 19:45:15 -0600

    Maquetado inicial de aplicacion; maquetado inicial de comportamiento de update y delete; archivos de sobra eliminados

--- rick-y-morty/src/App.js
+++ rick-y-morty/src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios'
+import Characters from './components/Characters';
 
 function App() {
+
+  const [characters, setCharacters] = useState([])
+  const [info, setInfo] = useState([])
+
+  const getCharacters = async () => {
+    await axios.get('https://rickandmortyapi.com/api/character')
+    .then((result) => {
+      setInfo(result.data.info)
+      setCharacters(result.data.results)
+    })
+  } 
+
+  const changeText = (item) => {
+    document.getElementById(item.name + item.id).innerHTML = "Charlie"
+  }
+
+  const deleteItem = (item) => {
+    document.getElementById(item.id).innerHTML = ""
+  }
+
+  useEffect(() => {
+    getCharacters()
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Rick y Morty App
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <div className='container'>
+        {/* <Characters characters={characters}/> */}
+        <div className='row'>
+        {characters.map((item, index) => (
+            <div key={index} className='col mb-4' id={item.id}>
+                <div className='card' style={{minWidth: "200px"}}>
+                    <img src={item.image} alt=''/>
+                    <hr/>
+                    <div className='card-body'>
+                        <h5 className='card-title text-uppercase' id={item.name + item.id}>
+                            {item.name}
+                        </h5>
+                        <button type='button' className='btn btn-primary' onClick={() => {changeText(item)}}>
+                          Update  
+                        </button>
+                        <button type='button' className='btn btn-danger' onClick={() => {deleteItem(item)}}>
+                          Delete  
+                        </button>
+                    </div>
+                </div>
+            </div>
+        ))}        
+    </div>
+      </div>
     </div>
   );
 }
